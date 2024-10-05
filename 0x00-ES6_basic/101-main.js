bc133f12720ecc3d4a71edaa1e1102d68aad2b6d
assma-codes <assma.hm89@gmail.com>
2024-10-05 14:26:34 +0000

    basics

---
+++ 0x00-ES6_basic/101-main.js
@@ -0,0 +1,5 @@
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
+
